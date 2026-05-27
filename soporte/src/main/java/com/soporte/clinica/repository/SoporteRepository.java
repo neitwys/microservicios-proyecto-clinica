6fc82e38f9ec6dc1f37331872feddadda3e6e29c
Matias <mat.figueroav@duocuc.cl>
2026-05-26 22:52:59 -0400

    Se reemplazo ficha_clinica por historial_clinico

--- soporte/src/main/java/com/soporte/clinica/repository/SoporteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.soporte.clinica.repository;
-
-import com.soporte.clinica.model.Soporte;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SoporteRepository extends JpaRepository<Soporte, Long> {
-}
